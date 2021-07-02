60558d8f9f94bb5aa7e9bcc1ae07c7ff7a6d7e34
desishrdn <38233989+desishrdn@users.noreply.github.com>
2021-07-02 09:44:31 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, View, Dimensions, SafeAreaView } from 'react-native';
-import {MainPKMB, HeaderBox, FooterBox, SlideOne, ClientData} from './app/index'
+import {StyleSheet, Dimensions } from 'react-native';
+import Route from './app/index'
 
 
 const { width, height } = Dimensions.get('screen');
@@ -10,29 +10,14 @@ const screenHeight = Dimensions.get('screen').height
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <HeaderBox  ></HeaderBox>
-      {/* <Text>Open up App.js to start working on your app!</Text>  */}
-       
-       {/* <MainPKMB ></MainPKMB> */}
-       {/* <SlideOne></SlideOne> */}
-       <ClientData></ClientData>
-       {/* <FooterBox></FooterBox> */}
-       <StatusBar style="auto" />
-    </View>
+    
+    <Route />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "column",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    // paddingBottom: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-    backgroundColor: "#FFFFFF",
-    width: width,
-    height: height,
     flex:1,
-    // position="absolute",
   },
 });
